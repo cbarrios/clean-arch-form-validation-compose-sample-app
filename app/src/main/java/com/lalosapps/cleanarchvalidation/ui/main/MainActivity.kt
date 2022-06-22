16f8652648852a509532e0a89e1ea600afe7407a
cbarrios <lalobarrios16@yahoo.com>
2022-06-22 19:50:38 -0400

    master: ui + view model integration

--- app/src/main/java/com/lalosapps/cleanarchvalidation/ui/main/MainActivity.kt
+++ app/src/main/java/com/lalosapps/cleanarchvalidation/ui/main/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.lalosapps.cleanarchvalidation.ui.main
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.lalosapps.cleanarchvalidation.R
+import com.lalosapps.cleanarchvalidation.core.util.ValidationEvent
 import com.lalosapps.cleanarchvalidation.ui.theme.CleanArchValidationTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +28,150 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CleanArchValidationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp() {
+    // A surface container using the 'background' color from the theme
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        val viewModel = viewModel<MainViewModel>()
+        val state = viewModel.state
+        val context = LocalContext.current
+        LaunchedEffect(key1 = context) {
+            viewModel.validationEvents.collect {
+                when (it) {
+                    is ValidationEvent.Success -> {
+                        Toast.makeText(
+                            context,
+                            context.getText(R.string.registration_success),
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
+            }
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            // Email
+            TextField(
+                value = state.email,
+                onValueChange = {
+                    viewModel.onEvent(RegistrationFormEvent.EmailChanged(it))
+                },
+                isError = state.emailError != null,
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {
+                    Text(text = stringResource(R.string.email))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email
+                )
+            )
+            if (state.emailError != null) {
+                Text(
+                    text = state.emailError,
+                    color = MaterialTheme.colors.error,
+                    modifier = Modifier.align(Alignment.End)
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            // Password
+            TextField(
+                value = state.password,
+                onValueChange = {
+                    viewModel.onEvent(RegistrationFormEvent.PasswordChanged(it))
+                },
+                isError = state.passwordError != null,
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {
+                    Text(text = stringResource(R.string.password))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            if (state.passwordError != null) {
+                Text(
+                    text = state.passwordError,
+                    color = MaterialTheme.colors.error,
+                    modifier = Modifier.align(Alignment.End)
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            // Repeated password
+            TextField(
+                value = state.repeatedPassword,
+                onValueChange = {
+                    viewModel.onEvent(RegistrationFormEvent.RepeatedPasswordChanged(it))
+                },
+                isError = state.repeatedPasswordError != null,
+                modifier = Modifier.fillMaxWidth(),
+                placeholder = {
+                    Text(text = stringResource(R.string.confirm_password))
+                },
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password
+                ),
+                visualTransformation = PasswordVisualTransformation()
+            )
+            if (state.repeatedPasswordError != null) {
+                Text(
+                    text = state.repeatedPasswordError,
+                    color = MaterialTheme.colors.error,
+                    modifier = Modifier.align(Alignment.End)
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            // Accept Terms
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = state.acceptedTerms,
+                    onCheckedChange = {
+                        viewModel.onEvent(RegistrationFormEvent.AcceptTerms(it))
+                    }
+                )
+                Spacer(modifier = Modifier.width(8.dp))
+                Text(text = stringResource(R.string.accept_terms))
+            }
+            if (state.termsError != null) {
+                Text(
+                    text = state.termsError,
+                    color = MaterialTheme.colors.error
+                )
+            }
+            // Submit button
+            Button(
+                onClick = {
+                    viewModel.onEvent(RegistrationFormEvent.Submit)
+                },
+                modifier = Modifier.align(Alignment.End)
+            ) {
+                Text(text = stringResource(R.string.submit))
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview("Light mode")
+@Preview("Dark mode", uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun DefaultPreview() {
     CleanArchValidationTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
